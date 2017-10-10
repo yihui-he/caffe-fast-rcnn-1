6bf8a6c2ac7e890bdb21fd285f606bfbaae18d23
Yihui He <yihuihe@foxmail.com>
2017-10-10 18:46:04 -0400

    kmenas

--- include/caffe/layers/kmeans_layer.hpp
+++ include/caffe/layers/kmeans_layer.hpp
@@ -16,7 +16,7 @@ template <typename Dtype>
 class KmeansLayer : public cluster_loss_layer<Dtype> {
  public:
   explicit KmeansLayer(const LayerParameter& param)
-    : cluster_loss_layer<Dtype>(param) {}
+    : cluster_loss_layer<Dtype>(param), diff_() {}
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
         const vector<Blob<Dtype>*>& top);    
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
