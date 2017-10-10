a75f6a05afb0a6f70eba0628613de05ccc09eba7
Yihui He <yihuihe@foxmail.com>
2017-10-10 04:43:45 -0400

    cluster

--- src/caffe/layers/cluster_loss_layer.cu
+++ src/caffe/layers/cluster_loss_layer.cu
@@ -141,7 +141,7 @@ void ClusterLossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     assign_matrix_back_.mutable_gpu_data()
     );
     Dtype loss;
-    caffe_gpu_asum(count_ass, loss_matrix_, &loss);
+    caffe_gpu_asum(count_ass, loss_matrix_.gpu_data(), &loss);
     top[0]->mutable_cpu_data()[0] = (Dtype)loss / spatial_size / bottom[0]->num()  / num_dims_;
 
     // merge diversity
